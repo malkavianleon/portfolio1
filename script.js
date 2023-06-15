bcf72a38e6837aa2fb0ad6b30115d1a3a442d65f
Leon <malkavianleon@gmail.com>
2023-06-15 11:03:47 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const hamburguer = document.querySelector(".hamburguer");
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".portfolio-navbar").classList.toggle("show");
+});
+
